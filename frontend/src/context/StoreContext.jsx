f70418d8bd0adb4447294a5e7919631daebfc7ff
abhishek <abhi96303@gmail.com>
2024-12-27 11:03:11 +0530

    changing the backend urls

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useEffect, useState } from "react";
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
-    const url = 'http://localhost:4000'
+    const url = 'https://forever-food-delivery-backend.onrender.com'
     const [token, setToken] = useState('')
   
     const [cartItems, setCartItems] = useState({})
